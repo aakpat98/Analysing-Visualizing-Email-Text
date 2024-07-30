62d18f932e10cf6277f8eb39103620185c118217
Aakash Patil <aakpat23998@gmail.com>
2024-07-30 20:36:26 +0530

    Project Push

---
+++ gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Month','umich.edu','ufp.pt','indiana.edu','ucdavis.edu','columbia.edu','uct.ac.za','mac.com','berkeley.edu','gmail.com','stanford.edu'],
+['2005',57,10,12,11,13,14,12,12,10,4],
+['2006',42,18,14,12,9,8,8,7,6,10]
+];
